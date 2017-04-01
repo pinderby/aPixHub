7e279a19be3e9f1cfd42e57345313e3f05c2e161
David Mangold <pinderby@gmail.com>
2017-04-01 10:23:33 -0700

    Section rendering and nested object rendering implemented

--- src/components/Section.js
+++ src/components/Section.js
@@ -5,15 +5,18 @@ import Helpers from '../helpers.js';
 class Section extends Component {
   render() {
     return (
-      <div className="section-container">
-        <div className="section">
-          
+      <div className="section-container panel panel-default">
+        <div className="panel-heading">
+          <h2 className="panel-title">{this.props.section.title}</h2>
+        </div>
+        <div className="section panel-body">
+          {this.props.section.content_body}
+          {Helpers.renderProps(this.props.section.properties)}
+          {Helpers.renderSections(this.props.section.sections)}
         </div>
       </div>
     );
   }
 }
-
-
 
 export default Section;
--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NodeProperty from './components/NodeProperty.js'
+import Section from './components/Section.js'
 
 class Helpers {
   static numberWithCommas(x) {
@@ -8,33 +9,69 @@ class Helpers {
     return parts.join(".");
   }
 
+  static renderSections(sections) {
+    if (typeof(sections) != "undefined") {
+      var sectionComponents = [];
+      sections.forEach(function(section) {
+        sectionComponents.push(<Section key={section.title} section={section} />);
+      });
+      return sectionComponents;
+    } else {
+      return;
+    }
+  }
+
   static renderProps(object) {
-    var props = [];
-    if (Object.prototype.toString.call( object ) === '[object Object]' ) {
-      for (var prop in object) {
-        if (object.hasOwnProperty(prop)) {
-          switch(typeof(object[prop])) {
-            case "string":
-                props.push(<NodeProperty key={prop} propKey={prop} value={object[prop]} type="string" />);
-                break;
-            case "number":
-                props.push(<NodeProperty key={prop} propKey={prop} value={object[prop]} type="number" />);
-                break;
-            case "object":
-                if (Object.prototype.toString.call( object[prop] ) === '[object Array]' ) {
-                  props.push(<NodeProperty key={prop} propKey={prop} value={object[prop].join(', ')} type="array" />);
-                } else {
-                  props.push(<NodeProperty key={prop} propKey={prop} value={object[prop]} type="object" />);
-                }
-                break;
-            default:
-                return;
+    if (typeof(object) != "undefined") {
+      var props = [];
+      if (Object.prototype.toString.call( object ) === '[object Object]' ) {
+        for (var prop in object) {
+          if (object.hasOwnProperty(prop)) {
+            switch(typeof(object[prop])) {
+              case "string":
+                  props.push(<NodeProperty key={prop} propKey={prop} value={object[prop]} type="string" />);
+                  break;
+              case "number":
+                  props.push(<NodeProperty key={prop} propKey={prop} value={object[prop]} type="number" />);
+                  break;
+              case "object":
+                  if (Object.prototype.toString.call( object[prop] ) === '[object Array]' ) {
+                    console.log(object[prop]);
+                    console.log(object[prop][0]);
+                    if (Object.prototype.toString.call( object[prop][0] ) === '[object Object]' ) {
+                      props.push(Helpers.renderObjects(object[prop]));
+                    } else {
+                      props.push(<NodeProperty key={prop} propKey={prop} value={object[prop].join(', ')} type="array" />);
+                    }
+                  } else {
+                    props.push(<NodeProperty key={prop} propKey={prop} value={object[prop]} type="object" />);
+                  }
+                  break;
+              default:
+                  return;
+            }
           }
         }
+        return props;
       }
-      return props;
+      return object;
     }
-    return object;
+    return;
+  }
+
+  static renderObjects(objects) {
+    console.log("objects: ", objects);
+    var explodedObjects = [];
+    if (typeof(objects) != "undefined") {
+      objects.forEach(function(object) {
+        console.log("object: ", object);
+        console.log("Helpers.renderProps(object): ", Helpers.renderProps(object));
+        explodedObjects.push(Helpers.renderProps(object));
+      });
+      console.log("explodedObjects: ", explodedObjects);
+      return explodedObjects;
+    }
+    return;
   }
 
   static formatPropKey(string) {
