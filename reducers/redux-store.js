c9bef083c02f888941c68ffc6f25c4894a56d8f4
Joe DiMaria <joe.a.dimaria@gmail.com>
2017-04-01 21:23:14 -0700

    Added back redux-store with no issues

--- reducers/redux-store.js
+++ reducers/redux-store.js
@@ -1,21 +1,56 @@
-import { SET_APIX_NODE, SET_NODE_TEMPLATE } from '../controllers/actions'
-import createStore from 'redux'
+import { combineReducers } from 'redux'
+import { 
+    REQUEST_APIX_NODE, REQUEST_NODE_TEMPLATE, 
+    SET_APIX_NODE, SET_NODE_TEMPLATE, SET_NODES
+} from '../controllers/actions'
 
-var apixReducer = (state = {}, action) => {
+const initialState = {
+    selectedNode: null,
+    selectedTemplate: null,
+    isFetching: false
+}
+
+function selectedNode(state = {}, action) {
     switch(action.type) {
+        case REQUEST_APIX_NODE:
+            return Object.assign({}, state, { isFetching: true })
+
         case SET_APIX_NODE:
-            return { apixNode: action.apixNode }
-        
-        case SET_NODE_TEMPLATE:
-            return { nodeTemplate: action.nodeTemplate }
+            return Object.assign({}, state, { selectedNode: action.apixNode, isFetching: false })
         
         default:
             return state
     }
 }
 
-module.exports = {
-    configureStore: function(initialState) {
-        return createStore(apixReducer, initialState)
+function selectedTemplate(state = {}, action) {
+    switch(action.type) {
+        case REQUEST_NODE_TEMPLATE:
+            return Object.assign({}, state, { isFetching: true })
+        
+        case SET_NODE_TEMPLATE:
+            return Object.assign({}, state, { selectedTemplate: action.nodeTemplate, isFetching: false })
+        
+        default:
+            return state
     }
 }
+
+function nodes(state = {}, action) {
+    console.log('Received nodes response')
+    switch(action.type) {
+        case SET_NODES:
+            return Object.assign({}, state, { nodes: action.nodes, isFetching: false })
+
+        default:
+            return state
+    }
+}
+
+const apixReducer = combineReducers({
+    selectedNode,
+    selectedTemplate,
+    nodes
+})
+
+export default apixReducer;
