91bb6ada730bba7204c9ae3dc9ff49270b011085
David Mangold <pinderby@gmail.com>
2017-09-06 22:27:21 -0700

    Added login logic to home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,64 @@
 import React, { Component } from 'react';
+import Auth from '../../services/Auth.js';
 import logo from '../../assets/apixhub-icon.svg';
 import './Home.css';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+        
+    // Instantiate Auth object
+    const auth = new Auth();
+
+    this.state = {
+      auth: auth,
+    };
+  }
+  
   render() {
+    // Instantiate authentication check from Auth0
+    const { isAuthenticated } = this.state.auth;
+
+    console.log("isAuthenticated(): ", isAuthenticated());
+    
+    // Instantiate body
+    let body = "";
+    if (!isAuthenticated()) { body = <Splash auth={this.state.auth} /> };
+
+    return (
+      <div className="home-container">
+        {body}
+      </div>
+    );
+  }
+}
+
+class Splash extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  login(e) {
+    // Begin auth0 auth process
+    this.props.auth.login();
+  }
+
+  logout(e) {
+    // Log out using auth0
+    this.props.auth.logout();
+  }
+
+  render() {
+    const { isAuthenticated } = this.props.auth;
+
     return (
       <div className="home">
         <div className="home-header-container">
           <div className="home-header">
             <img src={logo} className="home-logo" alt="logo" />
             <h2>Welcome to aPixHub</h2>
+            <button type="button" onClick={this.login} className="btn btn-success btn-lg">Sign up</button>
+            <button type="button" onClick={this.login} className="btn btn-success btn-lg">Log in</button>
           </div>
         </div>
         <p className="home-intro">
