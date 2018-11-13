40c82f7dfde559a99f4a266bf864dbb9f1519e52
David Mangold <pinderby@gmail.com>
2018-11-13 01:55:56 -0800

    Implemented basic search filter for nodes

--- src/components/NodesPanel/NodesPanel.js
+++ src/components/NodesPanel/NodesPanel.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { slide as Menu } from 'react-burger-menu';
-import { Button, Glyphicon } from 'react-bootstrap';
+import { Button, Glyphicon, FormControl } from 'react-bootstrap';
 import NodeInstancePopulator from '../NodeInstance/NodeInstancePopulator';
 import NodeSearchResult from '../NodeInstance/NodeSearchResult';
 
@@ -10,13 +10,30 @@ class NodesPanel extends Component {
 
     // Bind methods
     this.renderNodes = this.renderNodes.bind(this);
+    this.filterNodes = this.filterNodes.bind(this);
+    this.onQueryChanged = this.onQueryChanged.bind(this);
 
     // Initialize state
     this.state = {
       user: {},
+      query: "",
       editing: props.editing,
       node: props.node,
     };
+  }
+
+  // Update view with new search query
+  onQueryChanged(e) {
+    this.setState({
+      query: e.target.value
+    });
+  }
+
+  // Filter nodes based on search query
+  filterNodes(nodes) {
+    return nodes.filter(node => {
+      return node.properties.name.includes(this.state.query);
+    });
   }
 
   renderNodes(nodes, templateSettings) {
@@ -26,6 +43,9 @@ class NodesPanel extends Component {
 
     console.log('nodes: ', nodes);
     console.log('templateSettings: ', templateSettings);
+
+    // Filter nodes based on user query
+    nodes = this.filterNodes(nodes);
 
     // Return if not array (can occur when API call does not return nodes)
     if (Object.prototype.toString.call( nodes ) !== '[object Array]' ) return;
@@ -53,6 +73,14 @@ class NodesPanel extends Component {
           <Button bsStyle="primary" onClick={() => this.props.addNode()}>
             <Glyphicon glyph="plus" />
           </Button>
+        </div>
+        <div className="nodes-searchbar">
+          <FormControl 
+              className="searchbar" 
+              type="text" 
+              placeholder="Search"
+              value={this.state.query}
+              onChange={this.onQueryChanged} />
         </div>
         <div className="panel-body">
           {this.renderNodes(this.props.nodes, this.props.templateSettings)}
